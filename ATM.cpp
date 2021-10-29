d7ce4bd04626c97dcd23002a65b0170af9ab3678
Kirtesh Maskey <kirteshmaske123@gmail.com>
2021-10-29 21:32:52 +0530

    Added file and changed user interface

--- ATM.cpp
+++ ATM.cpp
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string>
+#include <fstream>
 #include <time.h>
 #include <windows.h>
+#define ll long long int
 using namespace std;
 
 class ATM
@@ -19,11 +21,30 @@ public:
     void showInformation();
     void depositMoney();
     void withdrawMoney();
-    void showBalance();
+    void showBalance(); // To show balance only
+    int accountNo();    // To return account Number to match with flie inorder to open it
+    int accountPass();  // To return account Password(PIN) to match with flie inorder to open it
+    string accountName();
 };
+
+int ATM::accountNo()
+{
+    return ACCNO;
+}
+
+int ATM::accountPass()
+{
+    return PIN;
+}
+
+string ATM::accountName()
+{
+    return NAME;
+}
 
 void ATM::newRegistration()
 {
+    BALANCE = 0.0;
     system("cls");
     cin.ignore();
     cout << "Your Full Name : ";
@@ -37,21 +58,21 @@ void ATM::newRegistration()
     system("cls");
     cout << "Account created sucessfully!!\n\n";
 
-    showInformation();
-    system("cls");
-
-    cout << "Do you want to deposit money (y/n)\n";
+    cout << "Do you want to deposit money (y/n) : "; // Ask to Deposit Money while creating account
     char ch;
     cin >> ch;
     if (ch == 'y' || ch == 'Y')
         depositMoney();
     else if (ch == 'n' || ch == 'N')
-        return;
+        ;
+    // return;
+
+    system("cls");
+    showInformation();
 }
 
 void ATM::showInformation()
 {
-
     cout << "Your account details are:\n";
     cout << "Account Name:  " << NAME;
     cout << "\nAccount number:  " << ACCNO;
@@ -62,17 +83,18 @@ void ATM::showInformation()
 
 void ATM::depositMoney()
 {
-    long long int depositAmount;
+    ll depositAmount;
     cout << "Enter amount to deposit:  ";
     cin >> depositAmount;
     BALANCE += depositAmount;
+
     system("cls");
     showBalance();
 }
 
 void ATM::withdrawMoney()
 {
-    long long int withdrawAmount;
+    ll withdrawAmount;
     cout << "Enter amount to withdraw:  ";
     cin >> withdrawAmount;
     if (withdrawAmount <= BALANCE)
@@ -102,11 +124,14 @@ void ATM::showBalance()
     cout << "Your account balance:  " << BALANCE;
     Sleep(4000);
 }
+
 int main()
 {
+    fstream details;
+    int pos;
     ATM user;
     srand(time(0));
-    
+
     int actionchoice;
     while (1)
     {
@@ -115,39 +140,109 @@ int main()
 
         cout << "\nEnter:\n";
         cout << "1. To create new account\n";
-        cout << "2. To show your account details\n";
-        cout << "3. to deposit money into your account\n";
-        cout << "4. To withdraw money from your account\n";
-        cout << "5. To display account balance\n";
-        cout << "6. To EXIT\n";
+        cout << "2. To login to your account\n";
+        cout << "3. To EXIT\n";
         cin >> actionchoice;
         system("cls");
         switch (actionchoice)
         {
         case 1:
             user.newRegistration();
+            details.open("accountDetails.dat", ios::app | ios::binary);
+            details.write((char *)&user, sizeof(user));
+            details.close();
             break;
 
         case 2:
-            user.showInformation();
+        AGAIN:
+            details.open("accountDetails.dat", ios::in | ios::out | ios::binary);
+            ll accNo;
+            int pin;
+            cout << "Enter your account number:  ";
+            cin >> accNo;
+            cout << "Enter 4 digit pin:  ";
+            cin >> pin;
+            details.seekg(0);
+            while (details.read((char *)&user, sizeof(user)))
+            {
+                pos = details.tellg();
+                if (user.accountNo() == accNo && user.accountPass() == pin)
+                {
+                    while (1)
+                    {
+                        system("cls");
+                        int choice;
+                        cout << "\n\t\t    BANK OF KIRTESH\n\n";
+                        string name = user.accountName();
+                        cout << "\t\tWelcome " << name << "!!";
+                        cout << "\n\nEnter:\n";
+                        cout << "1. To see account details\n";
+                        cout << "2. To deposit Money\n";
+                        cout << "3. To withdraw Money\n";
+                        cout << "4. To see account Balance\n";
+                        cout << "5. To Exit\n";
+                        cin >> choice;
+
+                        system("cls");
+                        switch (choice)
+                        {
+                        case 1:
+                            user.showInformation();
+                            break;
+
+                        case 2:
+                            user.depositMoney();
+                            details.seekg(pos - sizeof(user));
+                            details.write((char *)&user, sizeof(user));
+                            break;
+
+                        case 3:
+                            user.withdrawMoney();
+                            details.seekg(pos - sizeof(user));
+                            details.write((char *)&user, sizeof(user));
+                            break;
+
+                        case 4:
+                            user.showBalance();
+                            break;
+
+                        case 5:
+                            details.close();
+                            goto EXIT;
+
+                        default:
+                            cout << "Please make a valid choice\n";
+                            Sleep(2000);
+                        }
+                    }
+                    details.close();
+                }
+            }
+            system("cls");
+            if (details.end)
+                cout << "\nINVALID ACCOUNT NUMBER OR PASSWORD\n";
+            Sleep(2000);
+            details.close();
+            system("cls");
+            cout << "Enter again or Exit (y/n) : ";
+            char ch;
+            cin >> ch;
+            if (ch == 'y' || ch == 'Y')
+            {
+                system("cls");
+                goto AGAIN;
+            }
+            else if (ch == 'n' || ch == 'N')
+                break;
+        EXIT:
             break;
 
         case 3:
-            user.depositMoney();
-            break;
-
-        case 4:
-            user.withdrawMoney();
-            break;
-
-        case 5:
-            user.showBalance();
-            break;
-
-        case 6:
             exit(0);
+
         default:
-            break;
+            cout << "Please make a valid choice\n";
+            Sleep(2000);
         }
     }
 
